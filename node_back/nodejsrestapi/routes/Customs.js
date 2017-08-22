1f0efe9a1fe677d2401c0286daa1df6723dee0de
Gr <gouravroy09@gmail.com>
2017-08-22 15:56:31 +0530

    asdasd

--- node_back/nodejsrestapi/routes/Customs.js
+++ node_back/nodejsrestapi/routes/Customs.js
@@ -1629,10 +1629,29 @@ api.post('/updateUser',function(req,res,next){
             }
             else{
                 if(result.id!=undefined){
-                    var query_string = "replace into users(id,emp_grade_code,username,fullname,email_id,emp_no,doj,emp_grade_id,emp_type_id) "+
-    "values(?,?,?,?,?,?,?,?,?)";
-        db.query(query_string,[result.id,req.body.GradeName,
-            req.body.UserName,reqbody.FullName,req.body.EmailID,req.body.EmpNo,req.body.Doj,req.body.GradeCode,req.body.EmpType],function(err){
+
+
+
+                    var query_string =   "replace into users(id,emp_grade_code,username,fullname,email_id,emp_no,doj,emp_grade_id,emp_type_id) values";
+
+    //if(i<recordset.recordset.length){
+       // console.log(recordset.recordset.length);
+        //for(var i =0;i < recordset.recordset.length;i++){
+            //var User = recordset.recordset[i];
+            query_string = query_string + "("+result.id+
+            stringify(req.body.GradeName) + "," + stringify(req.body.UserName)+","+stringify(req.body.FullName)+","+
+            stringify(req.body.EmailID)+","+stringify(req.body.EmpNo)+","+
+            stringify(req.body.Doj)+","+zeroGrade(req.body.GradeCode)+","+zeroGrade(req.body.EmpType)+")";
+        //}
+        //query_string = query_string.slice(0,-1);
+       // query_string =query_string ;
+        console.log(query_string);
+
+
+
+    //                 var query_string = "replace into users(id,emp_grade_code,username,fullname,email_id,emp_no,doj,emp_grade_id,emp_type_id) "+
+    // "values(?,?,?,?,?,?,?,?,?)";
+        db.query(query_string,function(err){
                 if(err)
                 {
                     res.end(error);
